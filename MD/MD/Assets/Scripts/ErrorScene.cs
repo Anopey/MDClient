c51cd21967960780857894bd56ddf7eeb9653d05
Ozan Ürkmez <arkantos250@gmail.com>
2020-05-30 13:22:28 +0300

    error management

---
+++ MD/MD/Assets/Scripts/ErrorScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ErrorScene : MonoBehaviour
+{
+
+    private static string errorText;
+
+    [SerializeField]
+    private Text errorUIText;
+
+    void Start()
+    {
+        errorUIText.text = errorText;
+    }
+
+    public static void LoadError(string text)
+    {
+        errorText = text;
+        SceneManager.LoadScene("ErrorScene");
+    }
+
+    public void SwitchSceneButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
