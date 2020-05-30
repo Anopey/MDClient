6d7fa89e24f2f2d6056930d5eddd14b4a1c4d249
Ozan Ürkmez <arkantos250@gmail.com>
2020-05-30 17:55:05 +0300

    basis of song

---
+++ MD/MD/Assets/Scripts/Game/Song.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Song : MonoBehaviour
+{
+
+
+
+    #region Drum Integration
+
+    public float GetHoldTolerance()
+    {
+        return 0.5f;
+    }
+
+    public float GetStartHoldTolerance()
+    {
+        return GetHoldTolerance() * 2;
+    }
+
+    public float GetPerfectTolerance()
+    {
+        return 0.2f;
+    }
+
+    public float GetDrumPoint()
+    {
+        return 500f;
+    }
+    #endregion
+
+}
